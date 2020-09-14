ce509262df59c68ac7021007d330a607d6bddba8
kyle <60389539+kylehalo@users.noreply.github.com>
2020-09-13 22:50:01 -0400

    Update commands.py

--- commands.py
+++ commands.py
@@ -1,6 +1,7 @@
 import click
 from flask.cli import with_appcontext
 from models import db, user, product, orders, wish_list
+from main import app
 import psycopg2
 
 @click.command(name='create_tables')
